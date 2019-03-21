979d299b09a5dcda2586eaa20f589739087d770c
NDK1410 <khanh.nd1410@gmail.com>
2019-03-21 09:48:22 +0700

    modeling_posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "home_pages#index"
+
+  resources :categories, only:[:show] do
+    resources :posts, only:[:show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
