4c73da1e3ff260c0dc677277a5da647f8af06314
NDK1410 <khanh.nd1410@gmail.com>
2019-06-23 16:45:24 +0700

    full-text-search

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  extend FriendlyId
+  friendly_id :title, use: [:slugged]
+  searchkick text_start: [:content]
 
   scope :newest_by_desc, -> { order "created_at DESC" }
   scope :newest_by_id, -> { order "posts.id DESC" }
   scope :posts_of_category, -> (category_id){ where category_id: category_id }
   scope :view_of_post, -> { order "posts.view DESC" }
  
-  extend FriendlyId
-  friendly_id :title, use: [:slugged]
 end
