7c4904a02fcb587e6af574de9b0c947e2897237f
NDK1410 <khanh.nd1410@gmail.com>
2019-07-19 13:18:33 +0700

    right-bar-and-increment-posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,31 @@ class PostsController < ApplicationController
   	@categories = Category.all
   	@current_category = Category.friendly.find params[:category_id]
   	@current_post = Post.friendly.find params[:id]
+    @right_bar_posts = Post.posts_of_category(@current_category).view_of_post.limit(4)
+    @recent_posts = Post.all.view_of_post.limit(5)
+
+  	# increase_view when reload
+    # @current_post.increment!(:view) or increment_view
+    increment_view
+  end
+
+  private
+
+  def increment_view
+    session_id = request.session_options[:id]
+    is_increment = false
+
+    if session_id.present?
+      session[:view_post] = {} unless session[:view_post]
+
+      if (session_id != session[:view_post]["post_id_#{@current_post.id}"])
+        is_increment = true
+        session[:view_post]["post_id_#{@current_post.id}"] = session_id
+      end
+    else
+      is_increment = true
+    end
+
+    @current_post.increment!(:view) if is_increment
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_025648) do
+ActiveRecord::Schema.define(version: 2019_07_19_052853) do
 
   create_table "categories", options: "ENGINE=MyISAM DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_025648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "view"
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
